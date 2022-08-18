3b8b86acc93f9e953c9b628e0ab9b956f7c931d0
nouralnoumas <109877190+nouralnoumas@users.noreply.github.com>
2022-08-18 23:51:23 +0300

    created files

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function BMI(weight, height){
 
+    let result = weight / (height * height)
+return result
+
+}
+console. log (BMI (10,10))
+
+function Status (bmi){
+
+if (bmi < 18.5) {
+    return "لديك نقص بالوزن "
+} 
+ else if (bmi >= 18.5 && bmi < 25) {
+    return "وزنك صحي "
+ }
+ else if (bmi >= 25) {
+    return "لديك زيادة في الوزن"
+
+ }
+ }
+
+function calculate (){
+    let weight =document.getElementById(`weight`).value
+    let height = document.getElementById(`height`).value
+    let bmi = BMI(weight,height)
+    let desc = Status(bmi)
+    let div = document.getElementById('result')
+    div.innerText = bmi + " == " + desc
+}
